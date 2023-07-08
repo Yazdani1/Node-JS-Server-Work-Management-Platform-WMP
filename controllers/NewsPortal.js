ef5a2e6b62a9ae3cb0b76216faced6a62c6c095e
Yazdani Chowdhury <yaz4dani@gmail.com>
2023-07-08 23:06:04 +0300

    WIP

--- controllers/NewsPortal.js
+++ controllers/NewsPortal.js
@@ -10,24 +10,20 @@ const NewsPortal = require("../model/NewsPortal");
 
 exports.createNewsForPortal = async (req, res) => {
   try {
-    const { title, des } = req.body;
 
+    const { title, des } = req.body;
     if (!title) {
       return res.status(422).json({ error: "Please add title" });
     }
-
     if (!des) {
       return res.status(422).json({ error: "Please add des" });
     }
-
     const newsPortalDetails = NewsPortal({
       title,
       des,
       slug: uuid.v4(),
     });
-
     const saveNewsPortalNews = await NewsPortal.create(newsPortalDetails);
-
     res.status(201).json(saveNewsPortalNews);
   } catch (error) {
     res.status(500).json({ error: "Something went wrong" });
